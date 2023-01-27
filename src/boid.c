36e372c32231ee823a358f19b4b85950a33fdad6
Sandro Mund <sandro.mund@consistec.de>
2023-01-27 12:28:39 +0100

    fix wrong render angles

--- src/boid.c
+++ src/boid.c
@@ -1,16 +1,24 @@
 #include "boid.h"
 #include "mathStuff.h"
+#include "constants.h"
 #include <SDL2/SDL2_gfxPrimitives.h>
+#include <stdlib.h>
 
 void boid_render(Boid* b, SDL_Renderer* r) {
   Vec2dD ptA = {BOID_LENGTH / 2, 0}; // the tip
   Vec2dD ptB = {-1 * (BOID_LENGTH / 2), BOID_WIDTH / 2}; // right rear
   Vec2dD ptC = {-1 * (BOID_LENGTH / 2), -1 * (BOID_WIDTH / 2)}; // left rear
 
-  // TODO make rotation dependent on boid's velocity
-  // rotate "points" //XXX
   const Vec2dD xAxis = {1, 0};
-  const int angle = DEG(vec2dd_angle(xAxis, b->velocity));
+  int angle = DEG(vec2dd_angle(xAxis, b->velocity));
+
+  // top right
+  if (b->velocity.y < 0) {
+    angle = 360 - angle;
+
+    angle %= 360;
+  }
+  
   ptA = vec2dd_rotate(ptA, angle);
   ptB = vec2dd_rotate(ptB, angle);
   ptC = vec2dd_rotate(ptC, angle);
